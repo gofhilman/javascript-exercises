5983cc2397911eb7bd58b4df769353ac2b01f51c
Hilman Fikry <hilman12fikry@gmail.com>
2024-12-02 12:25:23 +0700

    Finish exercise 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, nRepeat) {
+    if(nRepeat<0) {
+        return 'ERROR'
+    }
+    let repeatedWord = '';
+    for(let i=0; i<nRepeat; i++) repeatedWord += word;
+    return repeatedWord
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(oriString) {
+    let reversedString = '';
+    for(let i=oriString.length-1; i>=0; i--) reversedString += oriString[i];
+    return reversedString;
 };
 
 // Do not edit below this line
